152114a9a9c8a6956d037ef41f40c092da66b3a1
chankane <brawnychocolate@gmail.com>
2019-02-07 22:36:29 +0900

    Add uniform func

--- pyind/crossover.py
+++ pyind/crossover.py
@@ -6,3 +6,13 @@ def p2(ind0, ind1):
     end = np.random.randint(_len) + 1
     start = np.random.randint(end)
     return np.hstack((ind0[:start], ind1[start:end], ind0[end:]))
+
+
+def uniform(ind0, ind1):
+    mask = np.random.randint(2, size=ind0.shape[0])
+    return ind0 * mask + ind1 * np.logical_not(mask)
+
+if __name__ == "__main__":
+    ind0 = np.zeros(30, dtype=int)
+    ind1 = np.ones(30, dtype=int)
+    print(uniform(ind0, ind1))
