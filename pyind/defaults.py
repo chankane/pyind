242ba86266a9e93c40a060609c653def4f6432bd
chankane <brawnychocolate@gmail.com>
2019-02-17 15:49:46 +0900

    Update defaults.py

--- pyind/defaults.py
+++ pyind/defaults.py
@@ -4,25 +4,22 @@ from . import selection as sel
 
 
 CONF = {
-    "eval": {
-        #"func": None,
-    },
     "sel": {
         "func": sel.elite,
         # "func": sel.roulette
         "rate": 0.05,
-        "num": 10
+        "num": 10,
     },
     "xovr": {
         "func": xovr.p2,
-        "pb": 0.875
+        "pb": 0.875,
     },
     "mut": {
         "func": mut.flip_bit,
-        "pb": 0.0075
-        "delta": 1
+        "pb": 0.0075,
+        "delta": 1,
     },
-    "goal_ind": None
+    "goal_ind": None,
 }
 
 END_GEN = 40
